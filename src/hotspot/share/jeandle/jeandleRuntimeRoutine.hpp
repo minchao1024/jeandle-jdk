bd24fa3ed07131651bb7bd284101963060df9e41
MinChao <minchao04@163.com>
2026-08-18 15:52:32 +0800

    Update src/hotspot/share/jeandle/jeandleRuntimeRoutine.hpp
    Co-authored-by: taofengliu <taofeng_liu@outlook.com>
    Signed-off-by: MinChao <minchao04@163.com>

--- src/hotspot/share/jeandle/jeandleRuntimeRoutine.hpp
+++ src/hotspot/share/jeandle/jeandleRuntimeRoutine.hpp
@@ -403,7 +403,7 @@ class JeandleRuntimeRoutine : public AllStatic {
 
   static address search_landingpad(JavaThread* current);
 
-  // Array allocation routine
+  // Allocation routine
   static void new_instance(Klass* klass, JavaThread* current);
   static void new_array(Klass* array_type, int length, JavaThread* current);
   // Slow-path array allocation: resolves the array klass from the component-type mirror
